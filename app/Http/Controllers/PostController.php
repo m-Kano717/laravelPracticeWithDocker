e1a12438fd047ebda6f9be33e9bdc934d5a1769c
m-Kano717 <305353319+m-Kano717@users.noreply.github.com>
2026-07-16 10:46:50 +0900

    ログイン関連機能のルート修正

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,15 +11,15 @@ use Illuminate\Support\Facades\Mail;
 
 class PostController extends Controller
 {
-    public function index(){
-        if(session()->has("name")){
+    public function index() {
+        if (session()->has("name")) {
             return redirect("showTop");
-        }else{
-        return view("index");
+        } else {
+            return view("index");
         }
     }
 
-    public function logOn(Request $request){
+    public function logOn(Request $request) {
         $request->validate([
             "mail" => "required",
             "pass" => "required"
@@ -28,91 +28,92 @@ class PostController extends Controller
         $mail = $request->input("mail");
         $pass = $request->input("pass");
         $hitpost = User::where("mail", $mail)->first();
-        if($hitpost and $hitpost->delete_flag == 0){
-            if(Hash::check($pass, $hitpost->pass)){
+        if ($hitpost and $hitpost->delete_flag == 0) {
+            if (Hash::check($pass, $hitpost->pass)) {
                 session(["name" => $hitpost->nick_name]);
                 session(["type" => $hitpost->user_type]);
-                if(session()->has("login")){
+                if (session()->has("login")) {
                     return redirect("changeInfo");
-                }else{
+                } else {
                     return redirect("showTop");
-                };
-            }else{
+                }
+                ;
+            } else {
                 return back()
                     ->withErrors(["pass" => "メールアドレスかパスワードが違います"])
                     ->withInput();
             }
-        }else {
+        } else {
             return back()
                 ->withErrors(["mail" => "メールアドレスかパスワードが違います"])
                 ->withInput();
         }
-        
-        
+
+
     }
 
-    public function showTop(){
+    public function showTop() {
         $randomItems = items::with("categories")->inRandomOrder()->limit(3)->get();
 
-        return view("top", ["randomItems"=>$randomItems]);
+        return view("top", ["randomItems" => $randomItems]);
     }
 
-    public function logOut(Request $request){
+    public function logOut(Request $request) {
         $request->session()->invalidate();
         $request->session()->regenerateToken();
         return redirect("showTop");
     }
 
-    public function userInfo(){
+    public function userInfo() {
         $name = session("name");
         $userInfo = User::where("nick_name", $name)->first();
         return view("userInfo", ["userInfo" => $userInfo]);
     }
 
-    public function showItems(){
+    public function showItems() {
         $itemList = items::with("categories")->get();
 
         return view("itemList", ["itemList" => $itemList]);
     }
 
-    public function itemDetail(int $id){
+    public function itemDetail(int $id) {
         $item = items::with("categories")->find($id);
-        if(!$item){
+        if (!$item) {
             // return "お探しの商品は見つかりませんでした。<br><a href="url('/')"></a>";
             return "お探しの商品は見つかりませんでした。<br><a href='" . url('/') . "'>トップへ戻る</a>";
         }
         return view("itemDetail", ["items" => $item]);
     }
 
-    public function registUser(){
-        if(session()->has("mailFlag")){
+    public function registUser() {
+        if (session()->has("mailFlag")) {
             return redirect("regist");
         }
         return view("registForm");
     }
 
-    public function registCheck(Request $request){
+    public function registCheck(Request $request) {
         // $regNum = 0;
         // $randNum;
         // if(session()->has("mailFlag")){
         //     return redirect("regist");
         // }
-        if($request->input("hid") === "0"){
+        if ($request->input("hid") === "0") {
             // $regNum = 1;
             $randNum = sprintf("%04d", random_int(0, 9999));
-            session(["randNum"=>$randNum]);
+            session(["randNum" => $randNum]);
             $mail = $request->input("mail");
-            session(["mail"=>$mail]);
+            session(["mail" => $mail]);
             $hid = 1;
             Mail::to($mail)->send(new checkMail($randNum));
             return view("registForm", compact("hid"));
-        }elseif($request->input("hid") === "1"){
+        } elseif ($request->input("hid") === "1") {
             $randNum = session("randNum");
-            if($request->num == $randNum){
+            if ($request->num == $randNum) {
                 session()->forget("randNum");
-                session(["mailFlag"=>1]);
+                session(["mailFlag" => 1]);
                 return redirect("/regist");
-            }else{
+            } else {
                 return redirect("/registUser")
                     ->withErrors(["num" => "The Number is Wrong."])
                     ->withInput();
@@ -120,27 +121,27 @@ class PostController extends Controller
         }
     }
 
-    public function regist(){
-        if(!session()->has("mailFlag")){
+    public function regist() {
+        if (!session()->has("mailFlag")) {
             return redirect("registUser");
         }
         // session(["mail" => "mail@testmail"]);
         return view("regist");
     }
 
-    public function checkInfo(Request $request){
+    public function checkInfo(Request $request) {
         $request->validate([
             "first_name" => "required|max:20",
             "last_name" => "required|max:20",
             "pass" => "required",
             "nick_name" => "required|max:50",
             "address1" => "required",
-            "address2" => ["required","max:20","regex:/^[ぁ-んァ-ヶー一-龠]{2,8}$/u","regex:/[市区町村]$/u"],
+            "address2" => ["required", "max:20", "regex:/^[ぁ-んァ-ヶー一-龠]{2,8}$/u", "regex:/[市区町村]$/u"],
             "address3" => "required|max:100",
             "address4" => "required|max:100",
             "birth_date" => "required|date|before_or_equal:today",
             "sex" => "required",
-            "tel" => ["required","max:20","regex:/^0\d{9,10}$/"],
+            "tel" => ["required", "max:20", "regex:/^0\d{9,10}$/"],
             "user_type" => "required"
         ]);
 
@@ -152,73 +153,81 @@ class PostController extends Controller
         return view("checkInfo", compact("userInfo"));
     }
 
-    public function registComplete(Request $request){
+    public function registComplete(Request $request) {
         $userInfo = $request->all();
         $userInfo["mail"] = session("mail");
         $userInfo["pass"] = Hash::make($userInfo["pass"]);
 
-        User::create($userInfo); 
+        User::create($userInfo);
 
         return redirect("compView");
     }
 
-    public function compView(){
+    public function compView() {
         return view("compView");
     }
 
-    public function showBasket(){
-        if(session()->has("basketItems")){
+    public function showBasket() {
+        if (!session()->has("name")) {
+            return redirect("/");
+        }
+        if (session()->has("basketItems")) {
             $items = session("basketItems", []);
             return view("itemBasket", ["items" => $items]);
-        };
+        }
+        ;
 
         return view("itemBasket");
     }
 
-    public function addBasket(Request $request){
+    public function addBasket(Request $request) {
+        if (!session()->has("name")) {
+            return redirect("/");
+        }
         $item = items::where("id", $request->id)->first();
-        if(!isset(session("basketItems")[$item->item_name]?->order)){
-            if($item->stock < 1){
+        if (!isset(session("basketItems")[$item->item_name]?->order)) {
+            if ($item->stock < 1) {
                 return redirect("/showBasket")
-                ->withErrors(["order" => "在庫超過"])
-                ->withInput();
+                    ->withErrors(["order" => "在庫超過"])
+                    ->withInput();
             }
             $item->order = 1;
-        }else{
+        } else {
             $order = session("basketItems")[$item->item_name]->order;
             $order = $order + 1;
-            if($item->stock < $order){
+            if ($item->stock < $order) {
                 return redirect("/showBasket")
-                ->withErrors(["order" => "在庫超過"])
-                ->withInput();
+                    ->withErrors(["order" => "在庫超過"])
+                    ->withInput();
             }
             $item->order = $order;
-        };
+        }
+        ;
         $item["total_price"] = $item->price * $item->order;
         $items = session("basketItems", []);
         $items[$item->item_name] = $item;
         session(["basketItems" => $items]);
-        
+
         return redirect("/showBasket");
         // return session("basketItems")[$item->item_name];
     }
 
-    public function basketAllDelete(){
+    public function basketAllDelete() {
         session()->forget("basketItems");
         return redirect("/showBasket");
     }
 
-    public function itemDelete(Request $request){
+    public function itemDelete(Request $request) {
         $item = items::where("id", $request->id)->first();
         $order = session("basketItems")[$item->item_name]->order;
         $order = $order - 1;
         $item["total_price"] = $item->price * $order;
-        if($order <= 0){
-            session()->forget("basketItems.".$item->item_name);
-            if(count(session('basketItems', [])) == 0){
+        if ($order <= 0) {
+            session()->forget("basketItems." . $item->item_name);
+            if (count(session('basketItems', [])) == 0) {
                 session()->forget("basketItems");
             }
-        }else{
+        } else {
             $item->order = $order;
             $items = session("basketItems", []);
             $items[$item->item_name] = $item;
@@ -227,7 +236,7 @@ class PostController extends Controller
         return redirect("/showBasket");
     }
 
-    public function deleteUser(){
+    public function deleteUser() {
         $name = session("name");
         $user = User::where("nick_name", $name)->first();
         $user->delete_flag = 1;
@@ -238,24 +247,26 @@ class PostController extends Controller
         // return $user;
     }
 
-    public function changeInfo(Request $request){
+    public function changeInfo(Request $request) {
         // dd(session()->all());
-        if(!session()->has("login")){
+        if (!session()->has("login")) {
             session()->forget("name");
             session(["login" => 1]);
             return redirect("/");
-        };
-       
-        if(isset($request->return_flag)){
-            $userInfo = $request; 
-        }else{
+        }
+        ;
+
+        if (isset($request->return_flag)) {
+            $userInfo = $request;
+        } else {
             $name = session("name");
             $userInfo = User::where("nick_name", $name)->first();
-        };
+        }
+        ;
         return view("changeInfo", compact("userInfo"));
     }
 
-    public function changeCheck(Request $request){
+    public function changeCheck(Request $request) {
         // dd(session()->all());
         $request->validate([
             "first_name" => "required|max:20",
@@ -263,23 +274,23 @@ class PostController extends Controller
             "pass" => "required",
             "nick_name" => "required|max:50",
             "address1" => "required",
-            "address2" => ["required","max:20","regex:/^[ぁ-んァ-ヶー一-龠]{2,8}$/u","regex:/[市区町村]$/u"],
+            "address2" => ["required", "max:20", "regex:/^[ぁ-んァ-ヶー一-龠]{2,8}$/u", "regex:/[市区町村]$/u"],
             "address3" => "required|max:100",
             "address4" => "required|max:100",
             "birth_date" => "required|date|before_or_equal:today",
             "sex" => "required",
-            "tel" => ["required","max:20","regex:/^0\d{9,10}$/"],
+            "tel" => ["required", "max:20", "regex:/^0\d{9,10}$/"],
             "user_type" => "required"
         ]);
 
 
         $userInfo = $request->all();
-         session()->forget("login");
+        session()->forget("login");
 
         return view("changeCheck", compact("userInfo"));
     }
 
-    public function changeRegist(Request $request){
+    public function changeRegist(Request $request) {
         $userInfo = $request->all();
         $userInfo["pass"] = Hash::make($userInfo["pass"]);
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,28 +1,31 @@
 <!DOCTYPE html>
 <html lang="ja">
 @include("common.head")
+
 <body>
     <header>
-    <h1>PHP練習ショップ</h1>
+        <h1>PHP練習ショップ</h1>
     </header>
     <contents>
-    <p>ログイン</p>
-    <div class="form">
-    <form action="/formPractice" method="POST">
-        @csrf
-        @error("mail")
-        <p style="color: red;">{{$message}}</p>
-        @enderror
-        @error("pass")
-        <p style="color: red;">{{$message}}</p>
-        @enderror
-        メールアドレス：<input type="text" name="mail" value="{{ old('mail') }}" >
-        <br>
-        パスワード：<input type="password" name="pass" value="">
-        <br>
-        <input type="submit" value="送信">
-    </form>
-</div>
+        <p>ログイン</p>
+        <div class="form">
+            <form action="/formPractice" method="POST">
+                @csrf
+                @error("mail")
+                <p style="color: red;">{{$message}}</p>
+                @enderror
+                @error("pass")
+                <p style="color: red;">{{$message}}</p>
+                @enderror
+                メールアドレス：<input type="text" name="mail" value="{{ old('mail') }}">
+                <br>
+                パスワード：<input type="password" name="pass" value="">
+                <br>
+                <input type="submit" value="送信">
+            </form>
+        </div>
+        <br/><a href="{{url('/registUser')}}">新規登録</a>
     </contents>
 </body>
+
 </html>
