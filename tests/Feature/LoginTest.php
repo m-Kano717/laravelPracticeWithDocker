6cb8ada99e85097bf71b7039c6f16b9335755051
m-Kano717 <305353319+m-Kano717@users.noreply.github.com>
2026-07-24 09:58:59 +0900

    Testファイルの編集

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -6,15 +6,27 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class LoginTest extends TestCase
-{
+class LoginTest extends TestCase {
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    public function test_logIn(): void {
+        $this->seed();
+        $response = $this->post('/formPractice', [
+            "mail" => "mail@testmail",
+            "pass" => "1111",
+        ]);
 
-        $response->assertStatus(200);
+        $response->assertRedirect("/showTop");
+    }
+
+    public function test_logIn_Fail(): void {
+        $this->seed();
+        $response = $this->post('/formPractice', [
+            "mail" => "mail@testmail",
+            "pass" => "1234",
+        ]);
+
+        $response->assertSessionHasErrors("mes");
     }
 }
